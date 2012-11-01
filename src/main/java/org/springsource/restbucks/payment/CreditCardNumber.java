8434b990892c46e7eeed95765bbe9d16eb8f5d80
Oliver Gierke <info@olivergierke.de>
2012-11-01 22:43:25 +0100

    Polishing of CreditCardNumber.

--- src/main/java/org/springsource/restbucks/payment/CreditCardNumber.java
+++ src/main/java/org/springsource/restbucks/payment/CreditCardNumber.java
@@ -22,9 +22,9 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import org.springframework.util.Assert;
-
 /**
+ * Value object to represent a {@link CreditCardNumber}.
+ * 
  * @author Oliver Gierke
  */
 @Data
@@ -35,21 +35,29 @@ public class CreditCardNumber {
 	private static final String regex = "[0-9]{16}";
 
 	@Column(unique = true)
-	private String creditCardNumber;
+	private String number;
 
 	/**
 	 * Creates a new {@link CreditCardNumber}.
 	 * 
-	 * @param creditCardNumber must not be {@literal null} and be a 16 digit numerical only String.
+	 * @param number must not be {@literal null} and be a 16 digit numerical only String.
 	 */
-	public CreditCardNumber(String creditCardNumber) {
+	public CreditCardNumber(String number) {
 
-		Assert.notNull(creditCardNumber);
-
-		if (!creditCardNumber.matches(regex)) {
-			throw new IllegalArgumentException();
+		if (!isValid(number)) {
+			throw new IllegalArgumentException(String.format("Invalid credit card NUMBER %s!", number));
 		}
 
-		this.creditCardNumber = creditCardNumber;
+		this.number = number;
+	}
+
+	/**
+	 * Returns whether the given {@link String} is a valid {@link CreditCardNumber}.
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public static boolean isValid(String number) {
+		return number == null ? false : number.matches(regex);
 	}
 }
