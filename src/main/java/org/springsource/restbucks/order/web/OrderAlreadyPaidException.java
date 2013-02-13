06e4a4f6bf4c4469073dddffdaaa36ae186e74c8
Oliver Gierke <info@olivergierke.de>
2013-02-13 20:17:43 +0100

    Polished JavaDoc.

--- src/main/java/org/springsource/restbucks/order/web/OrderAlreadyPaidException.java
+++ src/main/java/org/springsource/restbucks/order/web/OrderAlreadyPaidException.java
@@ -1,14 +1,31 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springsource.restbucks.order.web;
 
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
+ * Exception being thrown in case an {@link Order} has already been paid and a payment is reattempted.
+ * 
  * @author Oliver Gierke
  */
 @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 public class OrderAlreadyPaidException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-
 }
