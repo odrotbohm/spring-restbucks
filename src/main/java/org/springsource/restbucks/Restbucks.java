baf8887642c95d1c66ba3c21a36aa41c1e2e38a6
Oliver Gierke <ogierke@gopivotal.com>
2014-02-25 08:42:24 +0100

    Upgraded to latest Spring Data release train.
    Upgrade to Spring 4.0.2, Spring Data JPA 1.5.0.RELEASE, Spring Boot 1.0.0.RC3, Spring HATEOAS 0.9.0.RELEASE, Lombok 1.12.4.

--- src/main/java/org/springsource/restbucks/Restbucks.java
+++ src/main/java/org/springsource/restbucks/Restbucks.java
@@ -18,7 +18,7 @@ package org.springsource.restbucks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
