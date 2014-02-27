e780f51d7836ccbc8f4f43055897af4d48d669d6
Oliver Gierke <ogierke@gopivotal.com>
2014-02-27 09:53:36 +0100

    Added summary projection for Orders.
    Removed explicit dependency on Spring HATEOAS to pull in the version Spring Data REST requires. This is to make sure the collection rels get rendered correctly when the projection is applied to a collection resource.

---
+++ src/main/java/org/springsource/restbucks/order/web/OrderProjection.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springsource.restbucks.order.web;
+
+import org.joda.time.DateTime;
+import org.springframework.data.rest.core.config.Projection;
+import org.springsource.restbucks.order.Order;
+import org.springsource.restbucks.order.Order.Status;
+
+/**
+ * Projection interface to render {@link Order} summaries.
+ * 
+ * @author Oliver Gierke
+ */
+@Projection(name = "summary", types = Order.class)
+public interface OrderProjection {
+
+	/**
+	 * @see Order#getOrderedDate()
+	 * @return
+	 */
+	DateTime getOrderedDate();
+
+	/**
+	 * @see Order#getStatus()
+	 * @return
+	 */
+	Status getStatus();
+}
