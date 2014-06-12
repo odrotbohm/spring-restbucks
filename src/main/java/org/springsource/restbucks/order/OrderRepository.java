8ee931cee83b4322857c1ce9dcde5e61d6030882
Oliver Gierke <ogierke@gopivotal.com>
2014-06-12 12:53:34 +0200

    Use OrderProjection as excerpt projection for Orders.
    See [0] for details.
    
    [0] https://github.com/spring-projects/spring-data-rest/commit/1034ffaeaca943467ada45b77e4b54590ff7ed2c

--- src/main/java/org/springsource/restbucks/order/OrderRepository.java
+++ src/main/java/org/springsource/restbucks/order/OrderRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 the original author or authors.
+ * Copyright 2012-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,13 +19,16 @@ import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springsource.restbucks.order.Order.Status;
+import org.springsource.restbucks.order.web.OrderProjection;
 
 /**
  * Repository to manage {@link Order} instances.
  * 
  * @author Oliver Gierke
  */
+@RepositoryRestResource(excerptProjection = OrderProjection.class)
 public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
 
 	/**
