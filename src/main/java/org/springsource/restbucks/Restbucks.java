db87c6089aec94782561a9a2de2e27dd4e3c0afa
Oliver Gierke <ogierke@pivotal.io>
2016-01-04 17:23:40 +0100

    #31 - Upgraded to Spring HATEOAS 0.20.0 snapshots.
    Allows us to use a relative URI with DefaultCurieProvider and automatically get it expanded to an absolute one.
    
    Related tickets: spring-projects/spring-hateoas#421

--- src/main/java/org/springsource/restbucks/Restbucks.java
+++ src/main/java/org/springsource/restbucks/Restbucks.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 the original author or authors.
+ * Copyright 2013-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ public class Restbucks {
 	public static String CURIE_NAMESPACE = "restbucks";
 
 	public @Bean CurieProvider curieProvider() {
-		return new DefaultCurieProvider(CURIE_NAMESPACE, new UriTemplate("http://localhost:8080/profile/{rel}"));
+		return new DefaultCurieProvider(CURIE_NAMESPACE, new UriTemplate("/docs/{rel}.html"));
 	}
 
 	/**
