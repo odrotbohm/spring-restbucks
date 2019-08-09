82e09155dad21e99826e45bf056bd1b5528fe8bc
Oliver Drotbohm <odrotbohm@pivotal.io>
2019-08-09 14:51:10 +0200

    #91 - Polishing.
    Extend license header dates into 2019.

--- server/src/main/java/org/springsource/restbucks/payment/CreditCardRepository.java
+++ server/src/main/java/org/springsource/restbucks/payment/CreditCardRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import org.springframework.data.repository.CrudRepository;
 interface CreditCardRepository extends CrudRepository<CreditCard, Long> {
 
 	/**
-	 * Returns the {@link CreditCard} assicaiated with the given {@link CreditCardNumber}.
+	 * Returns the {@link CreditCard} associated with the given {@link CreditCardNumber}.
 	 * 
 	 * @param number must not be {@literal null}.
 	 * @return
