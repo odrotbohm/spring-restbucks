120d856ccbe1181d65595df79770c4c1794dba22
Oliver Drotbohm <info@odrotbohm.de>
2025-02-21 15:21:50 +0100

    Switch to ModulithEventMetricsCustomizer API.
    We now use the ModulithEventMetricsCustomizer to augment the metrics gathered for application events crossing module boundaries.

---
+++ server/src/main/java/de/odrotbohm/restbucks/order/web/OrderObservabilityConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.odrotbohm.restbucks.order.web;
+
+import de.odrotbohm.restbucks.order.Order.OrderCreated;
+import de.odrotbohm.restbucks.order.Order.OrderLineItemCreated;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.modulith.observability.ModulithEventMetricsCustomizer;
+
+/**
+ * Additional customizations for counters produced for the
+ *
+ * @author Oliver Drotbohm
+ */
+@Configuration
+class OrderObservabilityConfiguration {
+
+	@Bean
+	ModulithEventMetricsCustomizer modulithEventMetricsCustomizer() {
+
+		return metrics -> {
+
+			metrics.customize(OrderCreated.class, (event, it) -> {
+				it.tags("location", event.location() != null ? event.location().name() : "NONE");
+			});
+
+			metrics.customize(OrderLineItemCreated.class, (event, it) -> {
+
+				var lineItem = event.lineItem();
+
+				it.tags("name", lineItem.getName());
+				it.tags("milk", lineItem.getMilk().name());
+				it.tags("size", lineItem.getSize().name());
+			});
+		};
+	}
+}
